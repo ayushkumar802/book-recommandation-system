82c913666637273b76c2ba008519343cc43ac796
Ayush Kumar <ayushkumarrio22@gmail.com>
2025-05-18 17:28:34 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import pickle
 import numpy as np
+import urllib.parse
+
+
 
 top50= pickle.load(open("top_50.pkl","rb"))
 
@@ -16,6 +19,7 @@ app = Flask(__name__)
 pt=pickle.load(open("pt.pkl",'rb'))
 books=pickle.load(open("books.pkl",'rb'))
 similarity=pickle.load(open("similarity.pkl",'rb'))
+print(top50.columns)
 
 
 @app.route('/')
@@ -27,25 +31,47 @@ def index():
                            votes=list(top50['no-rating'].values),
                            rating=list(top50['avg-rating'].values)
     )
-
 @app.route('/recommend')
 def recommend_ui():
-    return render_template('recommend.html')
+    return render_template('recommend.html', book_list=list(top50['Book-Title'].unique()))
 
-@app.route('/recommend_books',methods=['post'])
+@app.route('/recommend_books', methods=['POST'])
 def recommend():
-    user_input= request.form.get('user_input')
-    index=np.where(pt.index==user_input)
-    a=sorted(list(enumerate(similarity[index[0][0]])),key=lambda x: x[1],reverse=True)[1:6]
-    m=[]
-    for i in range(5):
-        l=[]
-        temp=books[books['Book-Title']==pt.index[a[i][0]]]
-        l.extend(temp.drop_duplicates('Book-Title')['Book-Title'])
-        l.extend(temp.drop_duplicates('Book-Title')['Book-Author'])
-        l.extend(temp.drop_duplicates('Book-Title')['Image-URL-M'])
-        m.append(l)
-    return render_template('recommend.html',data=m)
+    user_input = request.form.get('user_input')
+
+    if user_input not in pt.index:
+        # Handle invalid book input gracefully
+        return render_template('recommend.html', data=0, book_list=list(top50['Book-Title'].unique()))
+
+    index = np.where(pt.index == user_input)[0][0]
+    similarity_scores = list(enumerate(similarity[index]))
+    sorted_books = sorted(similarity_scores, key=lambda x: x[1], reverse=True)[1:23]  # top 5
+
+    recommendations = []
+    for i in sorted_books:
+        book_title = pt.index[i[0]]
+        book_data = books[books['Book-Title'] == book_title].drop_duplicates('Book-Title')
+
+        if not book_data.empty:
+            title = book_data['Book-Title'].values[0]
+            author = book_data['Book-Author'].values[0]
+            image_url = book_data['Image-URL-M'].values[0]
+            recommendations.append([title, author, image_url])
+
+    return render_template('recommend.html', data=recommendations, book_list=list(top50['Book-Title'].unique()))
+
+
+
+@app.route('/book/<path:title>')
+def book_detail(title):
+    title = urllib.parse.unquote(title)  # Decode URL-encoded title
+    book = books[books['Book-Title'] == title]
+
+    if book.empty:
+        return "Book not found", 404
+
+    book_info = book.iloc[0]
+    return render_template("book_detail.html", book=book_info)
 
 if __name__ == '__main__':
     app.run(debug=True)
