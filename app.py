af727aca2e352af6083d1ff3fc7ce677098c5d9c
Ayush Kumar <ayushkumarrio22@gmail.com>
2025-05-22 01:39:24 +0530

    initial commit

--- app.py
+++ app.py
@@ -2,24 +2,22 @@ from flask import Flask, render_template, request, redirect, url_for
 import pickle
 import numpy as np
 import urllib.parse
-
+import pandas as pd
 
 
 top50= pickle.load(open("top_50.pkl","rb"))
+reviews = pd.read_csv("popular_books_reviews_with_rating.csv")
+REVIEWS_CSV = "popular_books_reviews_with_rating.csv"
 
 app = Flask(__name__)
-# def trim_to_50_words(text):
-#     words=  text.split()
-#     return ' '.join(words[:5])
-# book_name=[]
-# for i in list(top50['Book-Title'].values):
-#     book_name.append(trim_to_50_words(i))
+
 
 
 pt=pickle.load(open("pt.pkl",'rb'))
 books=pickle.load(open("books.pkl",'rb'))
 similarity=pickle.load(open("similarity.pkl",'rb'))
-print(top50.columns)
+book_list = list(top50['Book-Title'].unique())
+np.random.shuffle(book_list)
 
 
 @app.route('/')
@@ -33,7 +31,7 @@ def index():
     )
 @app.route('/recommend')
 def recommend_ui():
-    return render_template('recommend.html', book_list=list(top50['Book-Title'].unique()))
+    return render_template('recommend.html', book_list=book_list)
 
 @app.route('/recommend_books', methods=['POST'])
 def recommend():
@@ -41,11 +39,11 @@ def recommend():
 
     if user_input not in pt.index:
         # Handle invalid book input gracefully
-        return render_template('recommend.html', data=0, book_list=list(top50['Book-Title'].unique()))
+        return render_template('recommend.html', data=0, book_list=book_list)
 
     index = np.where(pt.index == user_input)[0][0]
     similarity_scores = list(enumerate(similarity[index]))
-    sorted_books = sorted(similarity_scores, key=lambda x: x[1], reverse=True)[1:23]  # top 5
+    sorted_books = sorted(similarity_scores, key=lambda x: x[1], reverse=True)[0:24]  # top 5
 
     recommendations = []
     for i in sorted_books:
@@ -58,7 +56,9 @@ def recommend():
             image_url = book_data['Image-URL-M'].values[0]
             recommendations.append([title, author, image_url])
 
-    return render_template('recommend.html', data=recommendations, book_list=list(top50['Book-Title'].unique()))
+
+    return render_template('recommend.html', data=recommendations, book_list=book_list)
+
 
 
 
@@ -71,7 +71,39 @@ def book_detail(title):
         return "Book not found", 404
 
     book_info = book.iloc[0]
-    return render_template("book_detail.html", book=book_info)
+    isbn = list(map(str, book['ISBN']))
+    print(isbn)
+
+    r1 = reviews[reviews['ISBN'].isin(isbn)]['Review'].tolist()
+    r2 = reviews[reviews['ISBN'].isin(isbn)]['Rating'].mean()
+    print(r1,r2)
+
+
+    return render_template("book_detail.html", book=book_info, r1=r1, r2=r2)
+
+@app.route('/submit_review', methods=['POST'])
+def submit_review():
+    isbn = request.form['isbn']
+    review = request.form['review']
+    rating = int(request.form['rating'])
+
+    # Append new review to the global DataFrame
+    global reviews
+    new_entry = pd.DataFrame([[isbn, review, rating]], columns=['ISBN', 'Review', 'Rating'])
+    reviews = pd.concat([reviews, new_entry], ignore_index=True)
+
+    # Save back to CSV
+    reviews.to_csv(REVIEWS_CSV, index=False)
+
+    # Get the book title from the books DataFrame
+    title_row = books[books['ISBN'] == isbn]
+    if title_row.empty:
+        return "Book not found", 404
+
+    book_title = title_row.iloc[0]['Book-Title']
+    return redirect(url_for('book_detail', title=book_title))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
